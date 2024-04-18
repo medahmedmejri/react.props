4acdacc0a8bd972dc632c9657e3f438a7e6969f3
medahmedmejri <medahmedmejri2003@gmail.com>
2024-04-18 18:07:43 +0200

    done1

---
+++ hell/src/conponde/players.js
@@ -0,0 +1,19 @@
+players=[
+    {
+ Name:'Cristiano Ronaldo',
+    Team:'Nassr FC',
+    Nationality:'portugais',
+    JersyNamber:'7',
+    Age:'39 years.old',
+    Image:'https://c8.alamy.com/comp/2R04PP5/cristiano-ronaldo-of-al-nassr-fc-in-action-against-al-raed-fc-during-their-saff-roshn-saudi-pro-league-2023-24-match-day-24-at-al-awwal-park-stadium-on-april-29-2023-in-riyadh-saudi-arabia-photo-by-victor-fraile-power-sport-images-2R04PP5.jpg'
+
+},
+{
+    Name:'Lionel Messi',
+    Team:'Inter Miami CF',
+    Nationality:'argentin',
+    JersyNamber:'10',
+    Age:'36',
+    Image:'https://dims.apnews.com/dims4/default/15357c6/2147483647/strip/true/crop/3752x2501+0+0/resize/599x399!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F41%2F41%2Fbbbf98e81306a82f7bb7d43f6d37%2Fc11bc3222e5a4b56b195dafc08f1b4af',
+}
+]
