9d4b0cc8e44ee783d893ff2209e9b1437b2737f9
medahmedmejri <medahmedmejri2003@gmail.com>
2024-04-21 21:39:40 +0200

    job is done

--- hell/src/conponde/Bigdata.js
+++ hell/src/conponde/Bigdata.js
@@ -5,8 +5,7 @@
     Nationality:'portugais',
     JersyNamber:'7',
     Age:'39 years.old',
-    Image:'https://c8.alamy.com/comp/2R04PP5/cristiano-ronaldo-of-al-nassr-fc-in-action-against-al-raed-fc-during-their-saff-roshn-saudi-pro-league-2023-24-match-day-24-at-al-awwal-park-stadium-on-april-29-2023-in-riyadh-saudi-arabia-photo-by-victor-fraile-power-sport-images-2R04PP5.jpg'
-
+    Image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg/375px-Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg',
 },
 {
     Name:'Lionel Messi',
@@ -14,7 +13,23 @@
     Nationality:'argentin',
     JersyNamber:'10',
     Age:'36',
-    Image:'https://dims.apnews.com/dims4/default/15357c6/2147483647/strip/true/crop/3752x2501+0+0/resize/599x399!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F41%2F41%2Fbbbf98e81306a82f7bb7d43f6d37%2Fc11bc3222e5a4b56b195dafc08f1b4af',
+    Image:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Lionel_Messi_WC2022.jpg/375px-Lionel_Messi_WC2022.jpg',
+},
+{
+Name:'Diego Maradona',
+Team:'Gimnasia y Esgrima La Plata',
+Nationality:'Argentine',
+JersyNamber:'10',
+Age:'60.yearsold',
+Image:'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Argentina_celebrando_copa_%28cropped%29.jpg/330px-Argentina_celebrando_copa_%28cropped%29.jpg',
+},
+{
+    Name:'Pelé',
+    Team:' New York Cosmos',
+    Nationality:'Brésilienne',
+    JersyNamber:'10',
+    Age:'82.yearsold',
+    Image:'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Pele_con_brasil_%28cropped%29.jpg/375px-Pele_con_brasil_%28cropped%29.jpg',
 },
 ];
 export default  Bigdata;
